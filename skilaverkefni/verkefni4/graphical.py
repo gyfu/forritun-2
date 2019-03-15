861422a4e0565265370a8981e7c47464f5d03e84
Gyfu <hthor35@gmail.com>
2019-03-15 21:14:07 +0000

    done

--- skilaverkefni/verkefni4/graphical.py
+++ skilaverkefni/verkefni4/graphical.py
@@ -9,43 +9,45 @@ tk=Tk()
 canvas=Canvas(width=WIDTH,height=HEIGHT)
 tk.title("Kassar og shit")
 class Shape():
-    def __init__(self,pos1,pos2):
-        self.pos1=pos1
-        self.pos2=pos2
     def move(self):
-        canvas.move(self,4,5)
-    def returnParameters(self):
-        return [pos1,pos2]
+        canvas.move(self.item,self.speedX,self.speedY)
+        pos=canvas.coords(self.item)
+        if pos[3] >= HEIGHT or pos[1]<=0:
+            self.speedY=-self.speedY
+        elif pos[2] >= WIDTH or pos[0]<=0:
+            self.speedX=-self.speedX
 class Square(Shape):
-    def __init__(self,pos1,pos2):
-        Shape.__init__(self,pos1,pos2)
-        canvas.create_rectangle(pos1[0],pos1[1],pos2[0],pos2[1],fill="blue")
+    def __init__(self,speedX,speedY):
+        self.speedX=speedX
+        self.speedY=speedY
+        self.item=canvas.create_rectangle(10,10,100,100,fill="red")
 class Circle(Shape):
-    def __init__(self,pos1,pos2):
-        Shape.__init__(self,pos1,pos2)
-        canvas.create_oval(pos1[0],pos1[1],pos2[0],pos2[1],fill="red")
+    def __init__(self,speedX,speedY):
+        self.speedX=speedX
+        self.speedY=speedY
+        self.item=canvas.create_oval(30,30,10,10,fill="blue")
 class Triangle(Shape):
-    def __init__(self,pos1,pos2):
-        Shape.__init__(self,pos1,pos2)
-        canvas.create_polygon(pos1[0],pos1[1],pos2[0],pos2[1],randint(0,80),randint(0,64),fill="Green")
-        Shape.move(self)
-def shapeGenerator(objects):
-    for x in range(0,99):
-        pos1x=randint(0,80)
-        pos1y=randint(0,64)
-        pos2x=randint(0,80)
-        pos2y=randint(0,64)
-        typeOf=randint(0,2)
-        if typeOf == 0:
-            objects.append(Square([pos1x,pos1y],[pos2x,pos2y]))
-        elif typeOf == 1:
-            objects.append(Circle([pos1x,pos1y],[pos2x,pos2y]))
-        elif typeOf== 2:
-            objects.append(Triangle([pos1x,pos1y],[pos2x,pos2y]))
-objects=[]
-shapeGenerator(objects)
-#ob1=Square([100,200],[400,500])
-
+    def __init__(self,speedX,speedY):
+        self.speedX=speedX
+        self.speedY=speedY
+        self.item=canvas.create_polygon(10,50,30,30,60,60,fill="green")
 
 canvas.pack()
+
+items=[]
+for i in range(100):
+    typeOf=randint(0,2)
+    if typeOf==0:
+        items.append(Square(randint(1,10),randint(1,10)))
+    elif typeOf==1:
+        items.append(Circle(randint(1,10),randint(1,10)))
+    elif typeOf==2:
+        items.append(Triangle(randint(1,10),randint(1,10)))
+
+while True:
+    for x in items:
+        x.move()
+    tk.update()
+    time.sleep(0.03)
+
 tk.mainloop()
