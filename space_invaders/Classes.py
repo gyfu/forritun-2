b38acbda82e088630e5dfa5bab40607987c144de
Gyfu <hthor35@gmail.com>
2019-04-30 08:52:45 +0000

    space_invaders

--- space_invaders/Classes.py
+++ space_invaders/Classes.py
@@ -2,15 +2,18 @@
 import pygame
 BLACK=(0,0,0)
 class Entity():
-    def __init__(self,window,pos,size=(50,50)):
+    def __init__(self,window,pos):
         self.window=window
-        self.x=pos[0]
-        self.y=pos[1]
-        self.pos=(self.x,self.y)
-        self.size=size
+        self.pos=pos
+        self.size=(50,50)
         self.make=self.pos+self.size
-        pygame.draw.rect(window,BLACK,pygame.Rect(self.make))
-    def move(self):
-        press=pygame.key.get_pressed()
-        if press[pygame.K_RIGHT]:
-            self.x+=1
+class Player(Entity):
+    def __init__(self,window,pos):
+        Entity.__init__(self,window,pos)
+        pygame.draw.rect(self.window,BLACK,pygame.Rect(self.make))
+class Invader(Entity):
+    def __init__(self,window,pos,var=0):
+        Entity.__init__(self,window,pos)
+        self.var=var
+        if var==0:
+            pygame.draw.rect(self.window,BLACK,pygame.Rect(self.make))
