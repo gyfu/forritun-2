b43042fccf22f964bb0d787cc046ad3e01422b1c
Gyfu <hthor35@gmail.com>
2019-04-30 16:02:54 +0000

    sd

--- space_invaders/Classes.py
+++ space_invaders/Classes.py
@@ -1,19 +1,3 @@
 #Höfundur: Huginn Þór Jóhannsson
 import pygame
 BLACK=(0,0,0)
-class Entity():
-    def __init__(self,window,pos):
-        self.window=window
-        self.pos=pos
-        self.size=(50,50)
-        self.make=self.pos+self.size
-class Player(Entity):
-    def __init__(self,window,pos):
-        Entity.__init__(self,window,pos)
-        pygame.draw.rect(self.window,BLACK,pygame.Rect(self.make))
-class Invader(Entity):
-    def __init__(self,window,pos,var=0):
-        Entity.__init__(self,window,pos)
-        self.var=var
-        if var==0:
-            pygame.draw.rect(self.window,BLACK,pygame.Rect(self.make))
--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -2,13 +2,11 @@
 import pygame
 import time
 from random import randint
-from Classes import Entity, Player, Invader
+from Classes import *
 space=pygame.image.load("assets/background-1.png")
 pygame.init()
 window=pygame.display.set_mode((640,480))
 pygame.display.set_caption("Space Invaders!")
-x=10
-y=420
 running=True
 jumping=False
 airTime=10
@@ -20,28 +18,5 @@ while running:
         if event.type==pygame.QUIT or event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE:
             running=False
     keys=pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x>10:
-        x-=10
-    if keys[pygame.K_RIGHT] and x<580:
-        x+=10
-    if not(jumping):
-        if keys[pygame.K_UP] and y>10:
-            y-=10
-        if keys[pygame.K_DOWN] and y<420:
-            y+=10
-        if keys[pygame.K_SPACE]:
-            jumping=True
-    else:
-        if airTime > -10:
-            neg=1
-            if airTime < 0:
-                neg=-1
-            y-=(airTime **2) *0.5*neg
-            airTime-=1
-        else:
-            jumping=False
-            airTime=10
-    player=Player(window,(x,y))
-        
     pygame.display.flip()        
 pygame.quit()
